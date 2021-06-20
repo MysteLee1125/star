2991dfad5b9682a6d69e1d937a9439988b8c8b1d
MysteLee1125 <84845488+MysteLee1125@users.noreply.github.com>
2021-06-20 10:50:35 +0900

    Add files via upload

---
+++ rotate.js
@@ -0,0 +1,64 @@
+function rotatefront(){
+    var angle = ($('.front').data('angle') + 15) || 15;
+    $('.front').css({'transform': 'rotate(' + angle + 'deg)'});
+    $('.front').data('angle', angle);
+}
+
+function rotatefront2(){
+    var angle = ($('.front').data('angle')  -15) || -15;
+    $('.front').css({'transform': 'rotate(' + angle + 'deg)'});
+    $('.front').data('angle', angle);
+}
+
+
+
+
+
+var windowWidth = $(window).width();
+var windowHeight = $(window).height();
+
+for (var i = 0; i < 1000; i++){
+      var toLeft = Math.round(Math.random()*windowWidth);
+      var toTop = Math.round((Math.random()*windowHeight)/2);
+
+      $("<div/>")
+      .addClass("star-sm")
+      .attr("style","top:"+toTop+"px;left:"+toLeft+"px;")
+      .appendTo("#upper")
+
+      if (i%5==0) {
+            $("<div/>")
+            .addClass("star-md")
+            .attr("style","top:"+toTop+"px;left:"+toLeft+"px;")
+            .appendTo("#upper");
+      }else if (i%25==0) {
+            $("<div/>")
+            .addClass("star-lg")
+            .attr("style","top:"+toTop+"px;left:"+toLeft+"px;")
+            .appendTo("#upper");
+
+      }
+}
+
+for (var i = 0; i <500; i++) {
+      var toLeft = Math.round(Math.random()*windowWidth);
+      var toTop = Math.round((Math.random()*windowHeight)/2);
+
+      $("<div/>")
+      .addClass("star-sm")
+      .attr("style","top:"+toTop+"px;left:"+toLeft+"px;")
+      .appendTo("#lower")
+
+      if (i%5==0) {
+            $("<div/>")
+            .addClass("star-md")
+            .attr("style","top:"+toTop+"px;left:"+toLeft+"px;")
+            .appendTo("#lower");
+      }else if (i%25==0) {
+            $("<div/>")
+            .addClass("star-lg")
+            .attr("style","top:"+toTop+"px;left:"+toLeft+"px;")
+            .appendTo("#lower");
+
+      }
+}
